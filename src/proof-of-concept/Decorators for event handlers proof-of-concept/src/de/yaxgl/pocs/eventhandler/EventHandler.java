4e384dce1c45c34eb5ff26c3afcd954bc65207ee
Henrik Heimbuerger <henrik@heimbuerger.de>
2007-05-18 13:59:18 +0000

    <> renamed (as -> for) + added annotation target type (great idea, Flo!)

--- src/proof-of-concept/Decorators for event handlers proof-of-concept/src/de/yaxgl/pocs/eventhandler/EventHandler.java
+++ src/proof-of-concept/Decorators for event handlers proof-of-concept/src/de/yaxgl/pocs/eventhandler/EventHandler.java
@@ -2,6 +2,7 @@ package de.yaxgl.pocs.eventhandler;
 
 import java.lang.annotation.*;
 
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EventHandler {
 	EventType	type();
