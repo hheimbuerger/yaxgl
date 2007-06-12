0578ab85621a31265ac71bd37a292ebdd1293a2f
froth <froth@f46fceb8-782f-0410-a3e0-df605f034a5e>
2007-06-12 23:36:15 +0000

    * optional regex

--- src/dotnet/yaxgl/yaxgl/EventHandlerManager.cs
+++ src/dotnet/yaxgl/yaxgl/EventHandlerManager.cs
@@ -29,9 +29,11 @@ namespace de.yaxgl
                     {
                         EventType eventType = ((EventHandler)attribute).eventType;
                         string eventID = ((EventHandler)attribute).eventID;
-
+                        bool regexOn = ((EventHandler)attribute).regexOn;
+                        
                         // now we need to check whether the signature of the annotated method is long enough
-                        if (method.GetParameters().Length != 2)
+                        // 2 for nomal use 3 with option regexON=true or off=false standart is off
+                        if (method.GetParameters().Length < 2 && method.GetParameters().Length > 3)
                         {
                             //TODO: geeignete Exception werfen
                             throw new Exception("Event handler must have exactly 2 Arguments.");
@@ -40,12 +42,12 @@ namespace de.yaxgl
                         // we also need to check whether the signature of the annotated method is what we expect it to be
                         Type componentParameter = method.GetParameters()[0].ParameterType;
                         Type argParameter = method.GetParameters()[1].ParameterType;
-                        if ((componentParameter != typeof(Component) || componentParameter != typeof(Component)) || argParameter != typeof(EventArgs))
+                        if (componentParameter != typeof(Component) || argParameter != typeof(EventArgs))
                         {
                             throw new Exception("Event Handler must have 2 Arguments with Type " + typeof(EventType).ToString() + " and " + typeof(string).ToString() + ".");
                         }
 
-                        EventHandlerMethod eventHandlerMethod = new EventHandlerMethod(eventReciever, method, eventType, eventID);
+                        EventHandlerMethod eventHandlerMethod = new EventHandlerMethod(eventReciever, method, eventType, eventID,regexOn);
                         eventHandlerMethods.Add(eventHandlerMethod);
                     }
                 }
