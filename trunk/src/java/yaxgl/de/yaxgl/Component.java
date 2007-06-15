91a2066c5bb53874535fa3a3612d78d9ed50b3eb
froth <froth@f46fceb8-782f-0410-a3e0-df605f034a5e>
2007-06-15 14:16:09 +0000

    +the java project

---
+++ trunk/src/java/yaxgl/de/yaxgl/Component.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package de.yaxgl;
+
+import java.awt.Rectangle;
+
+/**
+ * @author Cort
+ * @author Flo
+ */
+public abstract class Component {
+	protected String ID;
+	protected org.eclipse.swt.widgets.Widget control=null;
+	
+	
+}
