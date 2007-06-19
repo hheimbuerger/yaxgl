bfd5a828bfbd2088e276c905e0bd82ca664999a2
froth <froth@f46fceb8-782f-0410-a3e0-df605f034a5e>
2007-06-19 21:32:34 +0000

    * fixed some bugs in EventHandling

--- src/java/TestApplication/Start.java
+++ src/java/TestApplication/Start.java
@@ -1,16 +1,39 @@
+import de.yaxgl.Base.Component;
 import de.yaxgl.Container.*;
+import de.yaxgl.EventDispatcher.EventArgs;
+import de.yaxgl.EventDispatcher.Attribute.EventHandler;
+import de.yaxgl.EventDispatcher.Attribute.EventType;
+import de.yaxgl.Helper.LookAndFeel;
 
 public class Start {
 
+	private WindowManager windowManager=null;
+	
 	public Start()
 	{
-		Window win=new Window("test.xml");
-		win.show();
+		try {
+			WindowManager.initialize(LookAndFeel.System);
+			windowManager=WindowManager.getInstance();
+			Window window=windowManager.createWindow("test.xml", this);
+			windowManager.run(window);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 	}
+	
+	@EventHandler(eventType=EventType.Click,eventID="button1",regexOn=false)
+	public void buttonClick(Component sender,EventArgs eventArgs)
+	{
+		System.out.println("Hallo");
+		
+	}
+	
+	
+	
 	
 	public static void main(String[] args) {
 		new Start();
 	}
-
-
 }
