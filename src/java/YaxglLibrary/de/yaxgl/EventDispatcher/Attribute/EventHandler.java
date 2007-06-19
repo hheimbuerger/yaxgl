3b794d3d0dc99b7a443cfed4a8967cf3fed7f814
froth <froth@f46fceb8-782f-0410-a3e0-df605f034a5e>
2007-06-19 13:24:42 +0000

    * EventHandlerManager implemented * EventHandlerMethod implemented

--- src/java/YaxglLibrary/de/yaxgl/EventDispatcher/Attribute/EventHandler.java
+++ src/java/YaxglLibrary/de/yaxgl/EventDispatcher/Attribute/EventHandler.java
@@ -1,5 +1,18 @@
 package de.yaxgl.EventDispatcher.Attribute;
 
-public @interface EventHandler {
+import static java.lang.annotation.ElementType.*;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.*;
+
+@Target(METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EventHandler {
+	
+	public EventType eventType();
+	public String eventID();
+	public boolean regexOn();
+	
+	//todo: wie ehier schon regexOn=false setzten standart
+	//und gehen auch so konstruktoren wi in c#
 }
--- src/java/YaxglLibrary/de/yaxgl/EventDispatcher/EventHandlerMethod.java
+++ src/java/YaxglLibrary/de/yaxgl/EventDispatcher/EventHandlerMethod.java
@@ -1,5 +1,70 @@
 package de.yaxgl.EventDispatcher;
 
-public class EventHandlerMethod {
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import de.yaxgl.Base.*;
+import de.yaxgl.EventDispatcher.Attribute.*;
+
+public class EventHandlerMethod {
+	 private Object eventReciever;
+     private Method methodInfo;
+     private EventType eventType;
+     private String eventID;
+     private boolean regexOn;
+
+     public EventHandlerMethod(Object eventReciever, Method methodInfo, EventType eventType,String eventID,boolean regexOn)
+     {
+         this.eventReciever = eventReciever;
+         this.methodInfo = methodInfo;
+         this.eventType = eventType;
+         this.eventID = eventID;
+         this.regexOn = regexOn;
+     }
+
+     
+     public boolean isMatching(String eventID)
+     {
+         boolean retVal;
+         if (regexOn)
+         {
+        	 Pattern p = Pattern.compile("^"+this.eventID+"$");
+        	 Matcher m = p.matcher(eventID);
+        	 retVal = m.matches();
+         }
+         else 
+         {
+             retVal=this.eventID.equals(eventID);   
+         }
+         return retVal;
+     }
+
+     public boolean isTypeOf(EventType eventType)
+     {
+         if (this.eventType == eventType)
+             return true;
+         else
+             return false;
+     
+     }
+
+     public void invokeMethod(Component component, EventArgs eventArgs)
+     {
+         Object[] arg ={ (Object)component, eventArgs };
+         try {
+			methodInfo.invoke(eventReciever, arg);
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+     }
 }
