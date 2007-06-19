35789b436ecc4234c5f59710720ad0aa1f18d11c
froth <froth@f46fceb8-782f-0410-a3e0-df605f034a5e>
2007-06-19 18:31:58 +0000

    + Demo Application

---
+++ trunk/src/java/TestApplication/Start.java
@@ -0,0 +1,16 @@
+import de.yaxgl.Container.*;
+
+public class Start {
+
+	public Start()
+	{
+		Window win=new Window("test.xml");
+		win.show();
+	}
+	
+	public static void main(String[] args) {
+		new Start();
+	}
+
+
+}
